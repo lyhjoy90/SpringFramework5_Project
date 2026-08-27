fa06132af268b05b1e41484ba581c5e03b52323c
lyhjoy90 <lyhjoy90@gmail.com>
2026-08-27 17:38:45 +0900

    놓침. 제미나이

--- src/test/java/myspring/di/xml/HelloBeanJunitTest.java
+++ src/test/java/myspring/di/xml/HelloBeanJunitTest.java
@@ -1,5 +1,42 @@
 package myspring.di.xml;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class HelloBeanJunitTest {
+	
+	ApplicationContext context;
+	
+	@BeforeEach
+	void setup() {
+		System.out.println("==> setup");
+		//1. Spring Container 객체생성
+		context = new GenericXmlApplicationContext("classpath:hello-bean.xml");
+		
+	}
+	
+	@Test
+	void helloBeanSetter() {
+		System.out.println("==> helloBeanSetter");
+		Hello hello1 = (Hello)context.getBean("hello");
+		Hello hello2 = context.getBean("hello",Hello.class);
+		
+		//주소 비교
+		System.out.println(hello1 == hello2);
+		assertSame(hello1, hello2);
+		
+		//값 비교
+		assertEquals("Hello 스프링", hello1.sayHello());
+		
+		hello1.print();
+		
+		Printer printer = context.getBean("strPrinter",Printer.class);
+		assertEquals("Hello 스프링", printer.toString());
+	}
+	
+	
 
 }
---
+++ src/test/java/myspring/di/xml/HelloBeanSpringTest.java
@@ -0,0 +1,35 @@
+package myspring.di.xml;
+
+import static org.junit.jupiter.api.Assertions.*;
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:hello-bean.xml")
+public class HelloBeanSpringTest {
+	@Autowired
+	@Qualifier("helloC")
+	Hello hello;
+	
+	@Resource(name = "hello")
+	Hello helloSetter;
+	
+	@Test
+	void helloBeanCons(){
+		//생성자
+		System.out.println(hello.sayHello());
+		//스프링
+		System.out.println(helloSetter.sayHello());
+		
+		assertEquals("Hello 생성자", hello.sayHello());
+		assertEquals("Hello 스프링", helloSetter.sayHello());
+		
+	}
+	
+}
